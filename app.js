d6d5e65a9377fc61accd49493fd392560078f52f
rahmadnet <rahmadnasution2@gmail.com>
2021-06-20 13:54:44 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
+
+const uri = 'mongodb://127.0.0.1:27017/node-rest-shop';
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.Promise = global.Promise;
+
+app.use(morgan('dev'));
+app.use('/uploads',express.static('uploads'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req,res,next) =>{
+    res.header('Access-Control-Allow-origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, x-Requested-With, Conten-Type, Sccept, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-allow-Methods', 'PUT, POST, PATCH', 'DELETE', 'GET');
+        return res.status(200).json({})
+    }
+    next();
+});
+// Routes which should handle request
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+app.use('/users', userRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req,res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+})
+
+module.exports= app;
