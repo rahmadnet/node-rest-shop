6bc7cbe8ca32b31ba4af551422d045bff5829588
rahmadnet <rahmadnasution2@gmail.com>
2021-06-23 21:01:14 +0700

    populate

--- api/controllers/order.js
+++ api/controllers/order.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 exports.orders_get_all = (req, res, next) => {
     Order.find()
         .select('product quantity _id')
+        .populate('product', 'name')
         .exec()
         .then(docs => {
             res.status(200).json({
@@ -36,7 +37,7 @@ exports.orders_create_order = (req, res, next) => {
                 return res.status(404).json({
                     message: "Product not found"
                 });
-            }
+            };
             const order = new Order({
                 _id: mongoose.Types.ObjectId(),
                 quantity: req.body.quantity,
@@ -70,6 +71,7 @@ exports.orders_create_order = (req, res, next) => {
 
 exports.orders_get_order = (req, res, next) => {
 Order.findById(req.params.orderId)
+    .populate('product')
     .exec()
     .then(order => {
         if (!order) {
