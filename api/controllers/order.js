ec40a50205d9f3de7eff1a63f65336bcd575e4f1
rahmadnet <rahmadnasution2@gmail.com>
2021-06-23 21:18:26 +0700

    populate

--- api/controllers/order.js
+++ api/controllers/order.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 exports.orders_get_all = (req, res, next) => {
     Order.find()
         .select('product quantity _id')
-        // .populate('product', 'name')
+        .populate('product', 'name')
         .exec()
         .then(docs => {
             res.status(200).json({
@@ -71,7 +71,7 @@ exports.orders_create_order = (req, res, next) => {
 
 exports.orders_get_order = (req, res, next) => {
 Order.findById(req.params.orderId)
-    // .populate('product')
+    .populate('product')
     .exec()
     .then(order => {
         if (!order) {
